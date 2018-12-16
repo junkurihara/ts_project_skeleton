d634b25de0ba8260c2ca21e050e0eb6dbfcb3d7c
Jun Kurihara <kurihara@ieee.org>
2018-12-16 21:56:57 +0900

    fix: added scripts for testing

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,8 +1,15 @@
-import * as hello from '../src/index';
-import {expect} from 'chai';
+import chai from 'chai';
+const expect = chai.expect;
+import {getTestEnv} from './prepare';
+const env = getTestEnv();
+const hello = env.library;
+const message = env.message;
+const envName = env.envName;
 
-describe('Test skeleton', () => {
+describe(`${envName}: Test skeleton`, () => {
     it('Test 1', () => {
+        console.log(message);
+
         const val: string = hello.hello('omg');
         console.log(val);
         expect(typeof val === "string").to.be.true;
