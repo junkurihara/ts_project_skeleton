fbe9a082f0642c092090b384b299adc351bc5f98
Jun Kurihara <kurihara@ieee.org>
2018-12-27 17:37:57 +0900

    fix: fixed use of type definitions

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,8 @@ const millinum: number = 1545288535676620;
 const goHex: string = 'e00c7d64bcf77115';
 
 export function calc(): void {
-  const bnNum = (new BN(millinum)).mul(new BN(1000));
-  const bin = nwbo(bnNum, 8);
+  const bnNum: BN = (new BN(millinum)).mul(new BN(1000));
+  const bin: Uint8Array = nwbo(bnNum, 8);
   console.log(`Equal to Hexdump from Golang? ${jseu.encoder.arrayBufferToHexString(bin) === goHex}`);
 
   const recoveredNum: any = nwboReverse(bin);
@@ -19,15 +19,15 @@ export function calc(): void {
 }
 
 const nwbo = (num: any, len: number): Uint8Array => {
-  const arr = num.toArray('le', len);
+  const arr: number[] = num.toArray('le', len);
   return new Uint8Array(arr);
 };
 
-const nwboReverse = (arr: Uint8Array): any => {
-  const val = new BN(0);
-  const base = new BN(0x100);
+const nwboReverse = (arr: Uint8Array): BN => {
+  const val: BN = new BN(0);
+  const base: BN = new BN(0x100);
   for (let i: number = 0; i < arr.length; i++) {
-    const x = new BN(arr[i]);
+    const x: BN = new BN(arr[i]);
     val.iadd(x.mul(base.pow(new BN(i))));
   }
   return val;
